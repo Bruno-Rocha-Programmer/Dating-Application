efa66e3df0baafaa61c87ceb1f57b2dcbe432916
Bruno-Rocha-Programmer <robertbruno592@gmail.com>
2025-07-03 20:38:09 -0300

    pushing the backend and the frontend to github

---
+++ backend/DatingApp/Api/Controllers/MembersController.cs
@@ -0,0 +1,33 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MembersController(DbAppContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        {
+            var members = await context.Users.ToListAsync();
+
+            return members;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetMember(string id)
+        {
+            var member = await context.Users.FindAsync(id);
+
+            if (member == null) return NotFound();
+
+            return member;
+        }
+       
+    }
+}
---
+++ backend/DatingApp/Api/Data/DbAppContext.cs
@@ -0,0 +1,12 @@
+﻿using DatingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+    public class DbAppContext : DbContext
+    {
+        public DbAppContext(DbContextOptions options) : base(options){ }
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}
