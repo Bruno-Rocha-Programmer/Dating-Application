802cad0e66b9cb4b46382a0df85b0a08fad70731
Bruno-Rocha-Programmer <robertbruno592@gmail.com>
2025-07-04 11:53:42 -0300

    Added cors on the backend, installed tailwindcss and daisyui on the frontend and created a card component and made a request to the backend member api

--- backend/DatingApp/Api/Program.cs
+++ backend/DatingApp/Api/Program.cs
@@ -10,13 +10,11 @@ builder.Services.AddDbContext<DbAppContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 app.MapControllers();
 
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
+  private http = inject(HttpClient);
   protected title = 'frontend';
+  protected members = signal<any>([]);
+
+  async ngOnInit() {
+    this.members.set(await this.getMembers());
+  }
+
+  async getMembers() {
+    try {
+      return lastValueFrom(
+        this.http.get('http://localhost:5233/Weatherforecast')
+      );
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
 }
